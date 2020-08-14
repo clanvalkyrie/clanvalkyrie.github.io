3f62a295245b8624c510eff41b944a549a9e6251
Zunay <sweetxdrops@gmail.com>
2020-08-14 18:22:35 -0300

    Envío inicial

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    var navH = $('nav').height();
+    var barH = $('bar').outerHeight();
+    if (navH > barH) {
+        $('.cont').css({marginTop: barH});
+    }
+    else {$('.cont').css({paddingTop: barH});}
+    $('.i').click(function() {$('body, html').animate({scrollTop:0},1000);});   
+    var top = $('bar').offset();
+    $(document).scroll(function(){
+        var scrolled = $(document).scrollTop();
+        if (scrolled < top.top) {$('body').removeClass('stick');}
+        else {$('body').addClass('stick');}
+    });     
+
+});
