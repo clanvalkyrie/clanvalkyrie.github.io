2b27d86ed8a4a1501fda16fb66afa1b4adaaf659
Zunay <sweetxdrops@gmail.com>
2020-08-18 20:34:26 -0300

    se completaron y corrigieron db incompletas

--- js/story.js
+++ js/story.js
@@ -1,8 +1,32 @@
 $(document).ready(function(){
-    //$(document).scrollTop(115);
+    $(document).scrollTop(115);
 
     $(document).scroll(function(){
         //$(document).scrollTop(115);
+
+        var scrolled = $(document).scrollTop();
+        if (scrolled < 115) {
+        	$(document).scrollTop(115);
+        }
+         
     });     
 
 });
+
+
+
+/* if (closeDialog == true) {
+	// almacenar nextStory en temporal.
+}
+
+if (place == "auto") {
+	// Mantiene lugar por deplazamiento por movimientos.
+}
+
+if (setObjetive) {
+	// Objetivo completo. Se repite el id para tacharlo de la lista.
+}
+
+if (nextStory == []){
+	// Finaliza episodio.
+}
